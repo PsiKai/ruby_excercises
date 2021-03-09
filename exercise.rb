3b52b4befb7fed01d6fb1a805cfe657cfc837dfc
David Irvin <63161310+PsiKai@users.noreply.github.com>
2021-03-08 19:13:20 -0700

    Add files via upload

---
+++ exercise.rb
@@ -0,0 +1,34 @@
+require 'net/http'
+require "json"
+
+api = "https://api.github.com/users/dhh/events/public"
+
+escaped_uri = URI.escape(api)
+
+uri = URI.parse(escaped_uri)
+
+commits = JSON.parse(Net::HTTP.get(uri))
+
+score = 0
+
+commits.each do |commit|
+
+  case commit["type"]
+    when "IssuesEvent"
+      score += 7
+    when "IssueCommentEvent"
+      score +=6
+    when "PushEvent"
+      score += 5
+    when "PullRequestReviewCommentEvent"
+      score += 4
+    when "WatchEvent"
+      score += 3
+    when "CreateEvent"
+      score += 2
+    else
+      score += 1
+  end
+end
+
+puts score
